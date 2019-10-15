f8f2462c4cd345e6b764790c36cd2e741d6ecd71
Gustavo Bacagine <gustavobacagine@gmail.com>
2019-10-15 17:43:57 -0300

    levels header

---
+++ niveis.h
@@ -0,0 +1,28 @@
+/* niveis.h
+ * Biblioteca criada por Gustavo Bacagine
+ * contendo o prototipo dos níveis do jogo
+ * Quiz For Developers
+ * 
+ * Data: 11/10/2019 */
+
+#ifndef _NIVEIS_H
+#define _NIVEIS_H
+
+/* NÍVEL FÁCIL
+ *
+ * */ 
+void facil();
+
+/* NÍVEL MÉDIO
+ *
+ * */ 
+void medio();
+
+/* NÍVEL DÍFICIL (100 PERGUNTAS)
+ *
+ * */
+void dificil();
+
+void criador();
+
+#endif
